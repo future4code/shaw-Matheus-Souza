815267e9071538c84322ffb9a04df34b1ba2770d
unknown <matheus92as@gmail.com>
2022-02-14 17:02:15 -0300

    Atividades-index-js

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,76 @@
+/*
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+
+// 1 - será impresso no console:
+// 10
+// 10 5
+
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+
+// 2 - Sem executar não tenho certeza, mas é provavel que irá surgir alguma mensagem
+// de erro, pois c não foi  declarado, e isso também ira causar erro na linha em que
+// b = c, se algo for impresso, então só a ira aparece, que é 10.
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+// 3 -
+// let horasDia = prompt("Quantas horas você trabalha por dia?") //Guarda o numero de horas
+// let valorDia = prompt("Quanto você recebe por dia?") //Guarda o valor ganho
+// alert(`Voce recebe ${valorDia/horasDia} por hora`) // Imprime o resultado da divisão, informando o valor da hora de trabalho
+*/
+
+/*
+let nome                        // O console mostra duas mensagens de undefined, pois as duas variaveis
+let idade                       // ainda não receberam valor nenhum.
+console.log (typeof nome)
+console.log (typeof idade)
+nome = prompt ("Digite seu nome")
+idade = prompt ("Digite sua idade")
+console.log (typeof nome)
+console.log (typeof idade)
+console.log ("Olá",nome,", você tem", idade,"anos.")
+*/
+
+/*
+let agua = prompt ("Você já se hidratou?")
+let cadeira = prompt ("Você está sentado?")
+let almoco = prompt ("Você já almoçou?")
+console.log ("Você já se hidratou?", agua)
+console.log ("Você está sentado?", cadeira)
+console.log ("Você já almoçou?", almoco)
+*/
+
+/*
+let a = 55
+let b = 10
+let c
+console.log ("a",a,"b",b)
+c = b
+b = a
+a = c
+console.log ("O novo valor de a é",a)
+console.log ("O novo valor de b é",b)
+*/
+
+/*
+let primeiroNumero = prompt ("Digite um número")
+let segundoNumero = prompt ("Digite outro número")
+let primeiroConvert = Number (primeiroNumero)
+let segundoConvert = Number (segundoNumero)
+console.log ("O primeiro número somado ao segundo número resulta em:",primeiroConvert+segundoConvert)
+console.log ("O primeiro número multiplicado pelo segundo número resulta em:",primeiroConvert*segundoConvert)
+*/
