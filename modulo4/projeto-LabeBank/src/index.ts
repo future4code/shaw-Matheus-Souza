86bdd0d516775705095ef56235065b8afe4c0cea
Matheus Souza <matheus92as@gmail.com>
2022-06-05 19:19:51 -0300

    Adicição de função de adicionar saldo

--- modulo4/projeto-LabeBank/src/index.ts
+++ modulo4/projeto-LabeBank/src/index.ts
@@ -3,8 +3,8 @@ import cors from 'cors'
 
 type Extract = {
   value: number,
-  date: number,
-  description: string,
+  date: string,
+  description: string
 }
 
 type User = {
@@ -65,13 +65,13 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-// Para testar se o servidor está tratando os endpoints corretamente
-app.get("/ping", (req: Request, res: Response) => {
-  res.status(200).send("pong!")
-})
-
 app.listen(3003, () => {
   console.log('Servidor funcionando')
+})
+// Para testar se o servidor está tratando os endpoints corretamente
+
+app.get("/users", (req: Request, res: Response) => {
+  res.status(200).send(users)
 })
 
 app.post("/createAccount", (req: Request, res: Response) => {
@@ -163,9 +163,150 @@ app.get("/balance/:cpf", (req: Request, res: Response) => {
   }
 })
 
-app.put("/balance", (req: Request, res: Response) => {
-  res.status(200).send("pong!")
+app.put("/balance/:cpf", (req: Request, res: Response) => {
+  type AddBalance = {
+    addBalance:number
+  }
+  try{
+    const { addBalance }:AddBalance = req.body
+    if(addBalance <=0){
+      res.status(422)
+      throw new Error("Valor inserido não pode ser adicionado")
+    }
+    if( typeof addBalance !== "number"){
+      res.status(422)
+      throw new Error("Insira um numero para adicionar ao saldo!")
+    }
+    if(req.query.name === undefined){
+      res.status(422)
+      throw new Error("Nome do usuario precisa ser informado")
+    }else{
+      const [findName] = users.filter((user:User) => {
+        if(user.name === req.query.name){
+          return user
+        }
+      })
+      if(!findName){
+        res.status(409)
+        throw new Error("Nome inserido não existe")
+      }
+    }
+    const cpf:number = Number(req.params.cpf)
+    const [findCpf] = users.filter((user:User) => {
+      if(user.cpf === cpf){
+        return user
+      }
+    })
+    if(!findCpf){
+      res.status(409)
+      throw new Error("Cpf não encontrado")
+    }
+    const yearNow = new Date().getFullYear();
+    const monthNow = new Date().getMonth();
+    const dayNow = new Date().getDate();
+    const findUser = users.map((user:User) => {
+      if(user.name === req.query.name){
+        return {...user, balance: user.balance + addBalance, extract: [{
+          value: addBalance,
+          date: `${dayNow}/${monthNow}/${yearNow}`,
+          description: `Depósito no valor de ${addBalance}`,
+        }]}
+      }else{
+        return user
+      }
+    })
+    users = findUser
+    console.log(users)
+    const resposta = `Nome: ${findCpf.name}, Saldo atual: R$ ${findCpf.balance + addBalance}`
+    res.status(200).send(resposta)  
+  }catch(error:any){
+    res.send(error.message)
+  }
 })
-app.put("/payBill", (req: Request, res: Response) => {
-  res.status(200).send("pong!")
+
+app.put("/payBill/:cpf", (req: Request, res: Response) => {
+  try{
+    const { value, date, description }:Extract = req.body
+    if(value <=0){
+      res.status(422)
+      throw new Error("Valor inserido invalido")
+    }
+    if( typeof value !== "number"){
+      res.status(422)
+      throw new Error("Insira um numero para o valor a pagar!")
+    }
+    if( typeof date !== "string"){
+      res.status(422)
+      throw new Error("Insira a data no formato dd/mm/aaaa!")
+    }
+    if( typeof description !== "string"){
+      res.status(422)
+      throw new Error("Insira um texto para a descrição!")
+    }
+    if(req.query.name === undefined){
+      res.status(422)
+      throw new Error("Nome do usuario precisa ser informado")
+    }else{
+      const [findName] = users.filter((user:User) => {
+        if(user.name === req.query.name){
+          return user
+        }
+      })
+      if(!findName){
+        res.status(409)
+        throw new Error("Nome inserido não existe")
+      }
+    }
+    const cpf:number = Number(req.params.cpf)
+    const [findCpf] = users.filter((user:User) => {
+      if(user.cpf === cpf){
+        return user
+      }
+    })
+    if(!findCpf){
+      res.status(409)
+      throw new Error("Cpf não encontrado")
+    }
+    const conversorYear = (data:string) => {  
+      const ano = data.substring(6, 10) 
+      return ano
+    }
+    const conversorMonth = (data:string) => {
+      const mes = data.substring(3, 5)   
+      return mes
+    }
+    const conversorDay = (data:string) => {
+      const dia = data.substring(0, 2) 
+      return dia
+    }
+    const payYear:number = Number(conversorYear(date))
+    const payhMonth:number = Number(conversorMonth(date))
+    const payDay:number = Number(conversorDay(date))
+    const yearNow = new Date().getFullYear();
+    const monthNow = new Date().getMonth();
+    const dayNow = new Date().getDate();
+    if(payYear < yearNow || //Ano menor que o atual
+      (payYear === yearNow && payhMonth < monthNow) || //Mes menor que o atual
+      (payYear === yearNow && payhMonth === monthNow && payDay < dayNow)){ //Dia menor que o atual
+        res.status(422)
+        throw new Error("Informe um dia de pagamento para durante ou depois do dia atual")
+      }
+    // const findUser = users.map((user:User) => {
+    //   if(user.name === req.query.name){
+    //     return {...user, balance: user.balance + addBalance, extract: [{
+    //       value: value,
+    //       date: date,
+    //       description: description,
+    //     }]}
+    //   }else{
+    //     return user
+    //   }
+    // })
+    // users = findUser
+    console.log(users)
+    const resposta = `Nome: ${findCpf.name}, Saldo atual: R$ ${findCpf.balance - value}`
+    res.status(200).send(resposta)  
+  }catch(error:any){
+    res.send(error.message)
+  }
 })
