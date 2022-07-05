46336b3ffc86e302c82cd8b5cf5bab8087e41335
Matheus Souza <matheus92as@gmail.com>
2022-07-05 20:04:21 -0300

    Projeto Labook

---
+++ modulo5/projeto-Labook/src/index.ts
@@ -0,0 +1,11 @@
+import { app } from "./app"
+// import { signup } from './endpoints/signup'
+// import { createTask } from './endpoints/createTask'
+// import { getTaskById } from './endpoints/getTaskById'
+// import { login } from './endpoints/login'
+
+// app.post('/signup', signup)
+// app.post('/user/login', login)
+
+// app.put('/task', createTask)
+// app.get('/task/:id', getTaskById)
