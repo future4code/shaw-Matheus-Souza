6b704466a7817efa1e8da822457eabd37e30777c
Matheus Souza <matheus92as@gmail.com>
2022-07-20 18:16:23 -0300

    Ajuste em responsividade mobile

--- modulo6/projeto-loteria/src/components/Sorteio/styled.js
+++ modulo6/projeto-loteria/src/components/Sorteio/styled.js
@@ -2,10 +2,15 @@ import styled from "styled-components";
 
 export const MainContainer = styled.div`
     align-items: center;
-p{
+    p{
         background-color: white;
         margin: 5px;
         padding: 15px;
         border-radius: 50px;
+    }
+    @media screen and (min-device-width:320px) and (max-device-width:375px){
+        p{
+        padding: 10px;
+        }
     }
 `
